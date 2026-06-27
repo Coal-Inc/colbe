c6cbda70b310818d1fce9d75a9e927d8f3f2f60f
FlameFragsC <Malebochester@proton.me>
2026-06-27 22:26:32 +0200

    feat: add components date: 27/06/2026

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to the start</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Navbar, Hero, Features, Cta, Footer } from '$lib/index';
+</script>
+
+<Navbar />
+<Hero />
+<Features />
+<Cta />
+<Footer />
+
